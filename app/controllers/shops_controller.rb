28f482ac1cc91589a7368c3f6696bcd5f8e59bfa
Tarek El-Sherif <tarek.elsherif9@gmail.com>
2015-04-05 12:39:39 +0200

    #24 Created the Follow Shop button in the Shop page and fixed create method in the shop_controller

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,10 +1,15 @@
 class ShopsController < ApplicationController
   def index
-    # sorted is a lambda (small procedure) in the model
+    # sorted is a scope (small procedure) in the model
   	@shops = Shop.sorted
   end
 
   def follow
+    # Adds the shop to the current user's followed shops
+    @current_user = current_user
+    @shop = Shop.find(params[:id])
+    @current_user.followed_shops << @shop
+    render 'show'
   end
 
   def show
@@ -18,13 +23,15 @@ class ShopsController < ApplicationController
   def create
     # Instantiate a new object - shop_params is a private methode
     @shop = Shop.new(shop_params)
+    @user = current_user
+    @shop.user = @user
     # Save
     if @shop.save
       # If succeeds, redirect
       redirect_to(:action => 'index')
+      flash[:notice] = "Shop created successfully."
     else
       # If fails, redisplay
-      flash[:notice] = "Shop created successfully."
       render('new')
     end
   end
