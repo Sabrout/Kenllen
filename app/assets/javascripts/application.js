59b62cf54baa08281b4edb223944722309e38274
sabrout <omarsabrout@gmail.com>
2015-04-08 00:56:31 +0200

    Layout Beta

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,39 @@
+
+// Search Bar
+
+$(function()
+{
+	// Hide search wrap by default;
+	$(".search-wrap").hide();
+
+	$(".toggle-search").on("click", function(e) 
+	{
+		// Prevent default link behavior
+		e.preventDefault();
+
+		// Stop propagation
+		e.stopPropagation();
+
+		// Toggle search-wrap
+		$(".search-wrap").slideToggle(300, function()
+		{
+			// Focus on the search bar
+			// When animation is complete
+			$("#search-bar").focus();	
+		});
+	});
+
+	// Close the search bar if user clicks anywhere
+	$(document).click(function(e)
+	{
+		var searchWrap = $(".search-wrap");
+		
+		if(!searchWrap.is(e.target) && searchWrap.has(e.target).length === 0)
+		{
+			searchWrap.slideUp(300);	
+		}
+	});    
+});
+ 
+// Log In form
+
