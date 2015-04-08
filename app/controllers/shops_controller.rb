5d93b86e9aea157bdfe3831a19931f5b39e4a594
sabrout <omarsabrout@gmail.com>
2015-04-08 20:09:30 +0200

    Restore master

---
+++ app/controllers/shops_controller.rb
@@ -0,0 +1,101 @@
+class ShopsController < ApplicationController
+  def index
+    # sorted is a scope (small procedure) in the model
+  	@shops = Shop.sorted
+  end
+
+  def follow
+    # Adds the shop to the current user's followed shops
+    @current_user = current_user
+    @shop = Shop.find(params[:id])
+    if !current_user.shops.include?(@shop)
+    @current_user.followed_shops << @shop
+    end
+    flash[:notice] = "You are now following '#{@shop.shop_name}.'"
+    redirect_to(:action => 'show', :id => @shop.id)
+  end
+
+  def unfollow
+    # Removes the shop from the current user's followed shops
+    @current_user = current_user
+    @shop = Shop.find(params[:id])
+    @current_user.followed_shops.delete(@shop)
+    flash[:notice] = "You are no longer following '#{@shop.shop_name}.'"
+    redirect_to(:action => 'show', :id => @shop.id)
+  end
+
+  def show
+  	@shop = Shop.find(params[:id])
+    @item = @shop.items
+  end
+
+  def new
+    @shop = Shop.new(:shop_name => 'default')
+  end
+
+  def create
+    # Instantiate a new object - shop_params is a private methode
+    @shop = Shop.new(shop_params)
+    @user = current_user
+    @shop.user = @user
+    # Save
+    if @shop.save
+      # If succeeds, redirect
+      redirect_to(:action => 'index')
+      flash[:notice] = "Shop created successfully."
+    else
+      # If fails, redisplay
+      render('new')
+    end
+  end
+
+  def edit
+    # Check if current user already owns the shop in order to edit
+    @shop = Shop.find(params[:id])
+  end
+
+  def update
+    # Find an existing object
+    @shop = Shop.find(params[:id])
+    # Update the object
+    if @shop.update_attributes(shop_params)
+      # If succeeds, redirect
+      flash[:notice] = "Shop updated successfully."
+      redirect_to(:action => 'show', :id => @shop.id)
+    else
+      # If fails, redisplay
+      render('edit')
+    end
+  end
+
+  def delete
+    # Check if user already owns the shop inorder to delete it
+    @shop = Shop.find(params[:id])
+    if current_user.shops.include?(@shop)
+    else
+    render 'index'
+    end
+  end
+
+  def destroy
+    # Find an existing object and destroy it
+    if current_user.shops.include?(shop)
+    shop = Shop.find(params[:id]).destroy
+    item = shop.items
+    item.each do |item| 
+    Item.destroy(item)
+    end
+    flash[:notice] = "Shop '#{shop.shop_name}' deleted successfully."
+    redirect_to(:action => 'index')
+    else 
+    render 'index'
+    end
+  end
+
+  # Mass assignement parameter
+  # It raises an error if :subject is not present
+  private
+  def shop_params
+    params.require(:shop).permit(:shop_name, :shop_desc, :cover)
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,35 @@
+class User < ActiveRecord::Base
+  self.table_name = 'users'                         #setting referenced table in db
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable
+
+         # Associations
+    has_many :shops , :dependent => :destroy
+    has_and_belongs_to_many :followed_shops, class_name: "Shop", join_table: "shops_users"
+
+    validates :fname , :presence => {:message => "Firsname Field Cannot be blank"}
+    validates :fname , :length => { :maximum => 12 , :message => "Firstname Is Too Long(maximum is 12 characters)" }  #sets the maximum length of first name to 12
+
+    validates :lname , :presence => {:message => "Lastname Field Cannot be blank"} 
+    validates :lname , :length => { :maximum => 12 , :message => "Lastname Is Too Long(maximum is 12 characters)"}  #sets the maximum length of last name to 12
+
+    validates :uname , :presence => {:message => "Username Field Cannot be blank"} 
+    validates :uname , :allow_blank => true , :length => {:maximum => 12 , :message => "Username Is Too long (maximum is 12 characters)"} #sets the maximum length of user name to 12
+    validates :uname , :allow_blank => true , :uniqueness => { case_sensitive: false , :message => "Username Already Taken" } # validate uniqueness
+    validates :uname , :allow_blank => true , :length => { :minimum => 4, :message => "Username Is Too Shot (minimum is 4 characters)" } #sets the minimum length of user name to 4
+
+    validates :phone , :presence => {:message => "Phone Field Cannot be blank"}, :length => { :maximum => 12 , :message => "PhoneNumber Is Too Long(maximum is 12 numbers)" } #sets the maximum length of phone to 12
+    validates :password , :presence => { :message => "Password Field Can't Be Blank"} #password can't be blank
+    validates :password , :allow_blank => true , :length => { :maximum => 12 , :message => "Password Is Too Long(maximum is 12 characters)"} #sets the maximum length of password to 12
+    validates :password , :allow_blank => true , :length => { :minimum => 6, :message => "Password Is Too SHORT(minimum is 6 characters)"} #sets the minimum length of password to 6
+    validates_confirmation_of :password, {:message => "These passwords don't match."}
+
+
+    validates :email ,:presence => {:message => "Email Can't Be Blank"}
+    validates :email ,:allow_blank => true ,:uniqueness => {:message => "Email Already Registerd"}
+    validates_confirmation_of :email, {:message => "Please , Re-enter Your Email"}
+
+    mount_uploader :image , ImageUploader
+end
