138a6693d8f8244a11f998002fbcb6c8d535dddf
Omar Waleed <omarpersonalprofile@gmail.com>
2015-04-17 20:58:00 +0200

    applying new messaging gem with new controllers and views

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,45 +1,32 @@
 class MessagesController < ApplicationController
 
-  # before_filter :authenticate_user!
+  def show
+  end
 
-	# GET /message/new
+  def inbox
+
+  	if current_user == nil
+  		redirect_to new_user_session_path
+  	end
+
+  end
+
+  def trash
+  end
+
   def new
-    # @user = User.find(params[:user])
-    # @message = current_user.messages.new
   end
- 
-   # POST /message/create
-   # create a new message and sends it from current_user to the recipient given in the parameters
+
   def create
+  	@receiver = User.find_by(uname: params[:compose][:user])
 
-    @recipient = User.find_by(uname: params[:new_message][:runame])
-    # @recipient = User.find_by(one_param)
-    if @recipient != nil
-    current_user.send_message(@recipient, params[:new_message][:body], params[:new_message][:subject])
-    # current_user.send_message(message_params)
-    # flash[:notice] = 'Message has been sent!'
-    # redirect_to :conversations
-    # redirect_to root_path
-    else
-      flash[:alert] = 'Message Failed'
-      # redirect_to root_path
-    end
-    # redirect_to conversations_inbox_path
-  end
-
-#   def create
-#     recipients = User.where(id: params['recipients'])
-#     conversation = @current_user.send_message(recipients, params[:message][:body], params[:message][:subject]).conversation
-#     flash[:success] = "Message has been sent!"
-#     redirect_to conversation_path(conversation)
-#   end
-
-  # left temporarly to redirecting the view
-  def index
-
-    # redirect_to root_path
-    redirect_to :conversations
-
+  	if @receiver == nil
+  		flash.keep[:notice] = 'No User exist with that username'
+  		redirect_to compose_path
+  	else
+  		current_user.send_message(@receiver, params[:compose][:subject], params[:compose][:body])
+  		redirect_to inbox_path
+  	end
   end
 
 end
