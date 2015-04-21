215715d0e4a6ad37a8725e8576b5326b71b19913
Omar Waleed <omarpersonalprofile@gmail.com>
2015-04-21 19:00:49 +0200

    finalizing code comments and documentation #58

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,6 +5,8 @@ class MessagesController < ApplicationController
 
   def inbox
 
+    # checks if the current user is logged in or not, if not, he/she is redirected to the signin page
+
   	if current_user == nil
   		redirect_to new_user_session_path
   	end
@@ -18,6 +20,9 @@ class MessagesController < ApplicationController
   end
 
   def create
+
+    # on sending the message, the controller checks if the recepient exists, if he/she does exist, the message is sent
+    # else the page is rendered with a flash notice telling the current user that this user doesnt exist
   	@receiver = User.find_by(uname: params[:compose][:user])
 
   	if @receiver == nil
